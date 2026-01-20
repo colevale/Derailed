2e174c2499448a371a73dc7c8c93fc2e643c34ff
james-s1337 <james21s1337@gmail.com>
2026-01-20 10:36:15 -0500

    Optimized random minigame order a bit + some comments in MinigameManager

--- Assets/Scripts/Minigame Scripts/Minigame.cs
+++ Assets/Scripts/Minigame Scripts/Minigame.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95a8ff436c3341fb5e3dfdbe471396c241a39a33c995e43057a4b11b25d8756a
-size 1769
+oid sha256:0532df2bc66fa190540c0ddb14d16727b4eb13de43f441ca67cc204fd1e72b1d
+size 1899
--- Assets/Scripts/Minigame Scripts/MinigameManager.cs
+++ Assets/Scripts/Minigame Scripts/MinigameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c813d461e7fe7fa39c6e13769059d0c2303f03f469a83d3bd67a3d2e67edea76
-size 2161
+oid sha256:1dd83b3f81790f31390f1456665ab8698e1fda422bd84fb4bdea4fe9cf0d40de
+size 4432
