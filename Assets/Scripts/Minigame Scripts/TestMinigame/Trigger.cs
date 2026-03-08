012c59838636f3960a629007f736e7167ee87ab1
BlerkCodes <jc.kazuo@gmail.com>
2026-03-07 20:19:59 -0500

    Added Proper Minigame Scripts
    Added minigames, deleted existing minigames, TODO: see if interaction with scripts possible

---
+++ Assets/Scripts/Minigame Scripts/TestMinigame/Trigger.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:75df180fb1701593e802e4e81fee0e7485bfb5e1950b649bd92067719a38d1c9
+size 395
---
+++ Assets/Scripts/Minigame Scripts/TransitionScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:45d34738eb53ed07abc882b441091ce22debb07b16617bc14c0c98e8bf05ec39
+size 1167
