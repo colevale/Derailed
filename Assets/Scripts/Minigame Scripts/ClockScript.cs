a9f68de0c1cd673afad723c28bf441c527c822a3
BlerkCodes <jc.kazuo@gmail.com>
2026-04-28 14:20:49 -0400

    Added Win, Lose States + Other Features for Minigame Readability

---
+++ Assets/Scripts/Minigame Scripts/ClockScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e15e184f4115d2278051a2d1ff407c7af9d8c41c42b27e979f47057bdf892559
+size 1247
--- Assets/Scripts/Minigame Scripts/LevelTextScript.cs
+++ Assets/Scripts/Minigame Scripts/LevelTextScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a08f80db0d9b91f26467131981fd19f7a123d7c290560ff6fc6707f6d21d292f
-size 832
+oid sha256:dc7bf7d711b216ace53025b72e93bdc893699e8b72af9b5117170a7ff045b30f
+size 1084
