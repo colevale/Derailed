8f8b53b7f1d3abc87f8e2876f36fa0ad6e7ad627
BlerkCodes <jc.kazuo@gmail.com>
2026-04-30 16:05:29 -0400

    Fixed Level End Calculations

--- Assets/Scripts/LevelEnd.cs
+++ Assets/Scripts/LevelEnd.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6342a2018e1b3391dc381d2f74f842b55f442d8c2e59eecbaf732aa2a3efafd7
-size 5006
+oid sha256:6946b8896b78eae74052d7c0cf61c2dbfda645f2211248e5a28238b82a330927
+size 5120
