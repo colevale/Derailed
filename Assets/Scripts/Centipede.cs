f67f73fc3b2d2800aa1019a228fe6c8c24219994
BlerkCodes <jc.kazuo@gmail.com>
2026-04-30 20:44:53 -0400

    Fixed some test settings

--- Assets/Scripts/Centipede.cs
+++ Assets/Scripts/Centipede.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92ea2f5c784bfba5290a7a59edd2e846cd42e08416fac6c1970e16474b7ebc19
+oid sha256:61310b4eeecf0d46a95b74e93c495e20eefeda7ed87313997916d2fae0f05c3f
 size 1666
